e6b123370f891e4f0025edbefe2d76647dfade20
mbdev <mbdevm@gmail.com>
2024-10-06 02:46:38 +0200

    [WIP] Added missing coroutine in skyboxmanager for the skybox animation

--- Assets/ScriptsMarco/SkyboxManager.cs
+++ Assets/ScriptsMarco/SkyboxManager.cs
@@ -38,14 +38,35 @@ public class SkyboxManager : MonoBehaviour
         skyboxSphere.GetComponent<Renderer>().sharedMaterial.SetFloat("_Cutoff_Height", 1f);
 
     }
+
     public void SetSkyboxToVisible(int skyboxid)
     {
-        skyboxSphere.GetComponent<Renderer>().sharedMaterial.SetTexture("_Base_Texture", textures[skyboxid] );
         skyboxSphere.GetComponent<Renderer>().sharedMaterial.SetFloat("_Cutoff_Height", -1f);
-    }
+    }   
 
 
     void Start() {
         ResetSkyboxToHidden();
     }
+
+    public IEnumerator IncreaseFloatOverTime(float duration, float value, int skyboxid)
+    {
+        skyboxSphere.GetComponent<Renderer>().sharedMaterial.SetTexture("_Base_Texture", textures[skyboxid] );
+        float currentTime = 0f;
+        float startValue = skyboxSphere.GetComponent<Renderer>().sharedMaterial.GetFloat("_Cutoff_Height");
+        float targetValue = value;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float newValue = Mathf.Lerp(startValue, targetValue, currentTime / duration);
+            skyboxSphere.GetComponent<Renderer>().sharedMaterial.SetFloat("_Cutoff_Height", newValue);
+            yield return null;
+        }
+
+        // Ensure the value is exactly 1 at the end
+        skyboxSphere.GetComponent<Renderer>().sharedMaterial.SetFloat("_Cutoff_Height", targetValue);
+    }
+
+    
 }
